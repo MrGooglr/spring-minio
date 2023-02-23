8beb3d59faa131b676a4a7b1068a3203b3aa59a1
mrgooglr <34514544+MrGooglr@users.noreply.github.com>
2023-02-23 21:51:31 +0530

    initial commit

---
+++ src/main/java/com/aniket/minio/springdemo/MinioSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aniket.minio.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinioSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aniket/minio/springdemo/MinioSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniket.minio.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
